a6256573589043fc0d17aed84b7ddeaab24b28af
s1300142 <s1300142@std4dc46>
2023-06-13 13:04:06 +0900

    Won/lost message added

--- DiceG.c
+++ DiceG.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<string.h>
 int main(){
+  printf("What is your name?\n");
+  char na[15];
+  scanf("%s",na);
+  printf("Hello, %s!\n",na);
+  
   srand((unsigned int)time(NULL));
   printf("Rolling dice...\n");
   int n1=rand()%6+1;
@@ -9,6 +15,13 @@ int main(){
   int n2=rand()%6+1;
   printf("Die 2: %d\n",n2);
   printf("Total value: %d\n",n1+n2);
+  
+  if(n1+n2>7){
+    printf("You won!\n");
+  }
+  else{
+    printf("You lose!\n");
+  }
   return 0;
 }
   
